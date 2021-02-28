cfffe87dee1d6cf6f6fd312bd688e14a867e19e9
Lagaffe91 <56080971+Lagaffe91@users.noreply.github.com>
2021-02-28 13:13:01 +0100

    SDL2 preparations
    Prepared the project to support sdl2 and start to work on the graphical output.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
 #include "Chip8.h"
+#include "SDL.h"
 
 // TODO: Implement SDL and start to work on graphical output
 
 
 int main(int argc, char** argv)
 {
+
 	Chip8 Chip;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("ChipEmu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,64,32, NULL);
+	
 	
 	bool isRunning = true;
 
